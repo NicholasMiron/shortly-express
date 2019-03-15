c787f57f059e480c9f849ee87d4ed20912daeb7b
NicholasMiron <nicholasmiron@gmail.com>
2019-03-15 16:02:37 -0500

    Got some sign up tests to pass

--- server/app.js
+++ server/app.js
@@ -18,78 +18,92 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+  (req, res) => {
+    res.render('index');
+  });
 
 app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+  (req, res) => {
+    res.render('index');
+  });
 
 app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
+      });
+  });
 
 app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
 
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
         throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
   });
   
-  /************************************************************/
-  // Write your authentication routes here
-  /************************************************************/
+/************************************************************/
+// Write your authentication routes here
+/************************************************************/
   
-  app.post('/signup', (req, res) => {
-    console.log(req.body)
-    let saltedBody = models.Users.create(req.body);
-    saltedBody.then(() => {
-      res.statusCode = 201;
-      res.send();
-    })
-  });
+app.get('/signup', (req, res)=>{
+  res.render('signup');
+});
 
-  app.get('/signup', (req, res)=>{
-    res.render('signup')
+app.post('/signup', (req, res) => {
+  let users = models.Users.getAll();
+  users.then(allUserInfo => {
+    let userInDatabase = false;
+    for (let user of allUserInfo) {
+      if (user.username === req.body.username) {
+        userInDatabase = true;
+        break;
+      } 
+    }
+    if (userInDatabase) {
+      res.redirect('/signup');
+    } else {
+      let saltedBody = models.Users.create(req.body);
+      saltedBody.then(() => {
+        res.redirect('/');
+      });
+    }
   });
+});
+
+
   
 
 /************************************************************/
